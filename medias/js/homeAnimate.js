75db9d262b7ee191a2d02487b5c5a31e0b7cda29
imisslovelove <1064077399@qq.com>
2015-04-16 20:37:02 +0800

    Update homeAnimate.js

--- medias/js/homeAnimate.js
+++ medias/js/homeAnimate.js
@@ -25,7 +25,7 @@ $(function(){
         $('#w-post').fadeIn(200);
         $('#w-about').fadeIn(600);
         $('#w-tags').fadeIn(1000);
-        $('#w-none').fadeIn(1400);
+        $('#w-en').fadeIn(1400);
         $('#w-houngking').fadeIn(1800);
         $('#word').fadeIn(300, function() {
             $(this).empty().append('当你驻足这里，便是故事！');
@@ -38,7 +38,7 @@ $(function(){
     _.showWord($iPost, '故事的主角总是善意地提醒周围人恶意的目光', 'https://imisslovelove.github.io/cn');
     _.showWord($iAbout, '偶尔，他也总是自欺欺人！', 'https://imisslovelove.github.io/cn/about');
     _.showWord($iTags, '他却用温暖的内心感染着身边的每个人', 'https://imisslovelove.github.io/cn/tags');
-    _.showWord($iNone, '然后，他在你的心中驻留。', 'https://imisslovelove.github.io');
-    _.showWord($iNone, '阳光便成为你一生的行程。', 'https://imisslovelove.github.io/houngking');
+    _.showWord($iEn, '然后，他在你的心中驻留。', 'https://imisslovelove.github.io/en');
+    _.showWord($iHoungking, '阳光便成为你一生的行程。', 'https://imisslovelove.github.io/houngking');
 
 });
